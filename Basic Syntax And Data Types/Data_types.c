c0332a79bfe51a0f6db8b00a205e2e2a2d858e41
Rushilchauhan45 <chauhanrushil45@gmail.com>
2024-08-31 17:27:08 +0530

    1st session...

---
+++ Basic Syntax And Data Types/Data_types.c
@@ -0,0 +1,21 @@
+#include<stdio.h>//Standard input output header file
+#include<string.h>
+
+void main()
+{
+    //VAriables Declaration
+    int A = 10;
+    float B = 12.45;
+    double C = 13.454;
+    char D = 'A';
+    char E[10] = "Hellow";
+
+
+    //Printing way
+    printf("%d\n",A);
+    printf("%.2f\n",B);
+    printf("%.3lf\n",C);
+    printf("%c\n",D);
+    printf("%s\n",E);
+
+}
