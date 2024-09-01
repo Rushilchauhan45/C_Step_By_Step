40a173658ed7d70faf714321d995308c051d4929
Rushilchauhan45 <chauhanrushil45@gmail.com>
2024-09-01 22:16:17 +0530

    Changes Added...

---
+++ Basic Syntax And Data Types/demo2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main()
+{
+    int number;
+
+    printf("Enter the Number :");
+    scanf("%d", &number);
+
+    printf("Number : %d",number);
+    //to get value from console and give OP On Console
+}
